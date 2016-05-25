55859a6d2246d70ad8ece446004c751a07060e0d
jrrubalc <jason.ray.rubalcaba@gmail.com>
2016-05-24 19:06:57 -0700

    combine all twitter searches to one array

--- twitter_api.r
+++ twitter_api.r
@@ -4,30 +4,37 @@ library("twitteR")
 library("wordcloud")
 library("tm")
 source("credentials.r")
+source("score_sentiment.r")
 
 #to get your consumerKey and consumerSecret see the twitteR documentation for instructions
 setup_twitter_oauth(consumer_key,
                     consumer_secret,
                     access_token,
                     access_secret)
- 
+
 sunArtist = read.table("rockabilly.txt", header = TRUE)
 sunArtist=t(sunArtist)
 
+allTweets = vector(length=0)
+
 for (i in 1:length(sunArtist)) {
-  tweets<- searchTwitter(mydata[[1]], n=1500)
+  current_tweets<- searchTwitter(mydata[[i]], n=15)
+  allTweets = c(allTweets,current_tweets)
 }
+
+text = laply(allTweets, function(t) t$getText() )
+scores = score.sentiment(text, pos.words, neg.words, .progress='text')
 
 #save text
 r_stats_text <- sapply(r_stats, function(x) x$getText())
 #create corpus
 r_stats_text_corpus <- Corpus(VectorSource(r_stats_text))
- 
+
 #if you get the below error
 #In mclapply(content(x), FUN, ...) :
 #  all scheduled cores encountered errors in user code
 #add mc.cores=1 into each function
- 
+
 #run this step if you get the error:
 #(please break it!)' in 'utf8towcs'
 r_stats_text_corpus <- tm_map(r_stats_text_corpus,
@@ -38,4 +45,3 @@ r_stats_text_corpus <- tm_map(r_stats_text_corpus, content_transformer(tolower),
 r_stats_text_corpus <- tm_map(r_stats_text_corpus, removePunctuation, mc.cores=1)
 r_stats_text_corpus <- tm_map(r_stats_text_corpus, function(x)removeWords(x,stopwords()), mc.cores=1)
 wordcloud(r_stats_text_corpus)
-
