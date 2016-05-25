f5f4bb60ea5482fc6ef79e6e8200c2dc7c2d3d68
jrrubalc <jason.ray.rubalcaba@gmail.com>
2016-05-25 16:56:34 -0700

    remove word cloud sections

--- twitter_api.r
+++ twitter_api.r
@@ -1,5 +1,3 @@
-#http://davetang.org/muse/2013/04/06/using-the-r_twitter-package/
-
 library("twitteR")
 library("wordcloud")
 library("tm")
@@ -33,24 +31,3 @@ scores = score.sentiment(text, pos.words, neg.words, .progress='text')
 
 allTweets.df = ldply(allTweets, function(t) t$toDataFrame())
 write.csv(allTweets.df, file = "twitter_data.csv")
-
-#save text
-r_stats_text <- sapply(r_stats, function(x) x$getText())
-#create corpus
-r_stats_text_corpus <- Corpus(VectorSource(r_stats_text))
-
-#if you get the below error
-#In mclapply(content(x), FUN, ...) :
-#  all scheduled cores encountered errors in user code
-#add mc.cores=1 into each function
-
-#run this step if you get the error:
-#(please break it!)' in 'utf8towcs'
-r_stats_text_corpus <- tm_map(r_stats_text_corpus,
-                              content_transformer(function(x) iconv(x, to='UTF-8-MAC', sub='byte')),
-                              mc.cores=1
-                              )
-r_stats_text_corpus <- tm_map(r_stats_text_corpus, content_transformer(tolower), mc.cores=1)
-r_stats_text_corpus <- tm_map(r_stats_text_corpus, removePunctuation, mc.cores=1)
-r_stats_text_corpus <- tm_map(r_stats_text_corpus, function(x)removeWords(x,stopwords()), mc.cores=1)
-wordcloud(r_stats_text_corpus)
