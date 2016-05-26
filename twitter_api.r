afbfc75d8f64e31bf5566889672ae3de39ce4ddc
jrrubalc <jason.ray.rubalcaba@gmail.com>
2016-05-25 17:32:18 -0700

    fixes bug with search terms

--- twitter_api.r
+++ twitter_api.r
@@ -10,24 +10,29 @@ source("score_sentiment.r")
 pos.words = scan('positive-words.txt', what='character', comment.char=';')
 neg.words = scan('negative-words.txt', what='character', comment.char=';')
 
-#to get your consumerKey and consumerSecret see the twitteR documentation for instructions
+print("Setting up Twitter connection.")
 setup_twitter_oauth(consumer_key,
                     consumer_secret,
                     access_token,
                     access_secret)
 
-sunArtist = read.table("rockabilly.txt", header = TRUE)
-sunArtist=t(sunArtist)
+print("Reading in search phrases.")
+searchTerms = read.table("rockabilly.txt", header = TRUE)
+searchTerms=t(searchTerms)
 
 allTweets = vector(length=0)
 
+print("Searching Twitter")
 for (i in 1:length(sunArtist)) {
-  current_tweets<- searchTwitter(mydata[[i]], n=1500)
+  current_tweets<- searchTwitter(searchTerms[[i]], n=500)
   allTweets = c(allTweets,current_tweets)
 }
 
+print("Performing sentiment analysis.")
 text = laply(allTweets, function(t) t$getText() )
 scores = score.sentiment(text, pos.words, neg.words, .progress='text')
 
+print("Saving data to CSV.")
 allTweets.df = ldply(allTweets, function(t) t$toDataFrame())
 write.csv(allTweets.df, file = "twitter_data.csv")
+
